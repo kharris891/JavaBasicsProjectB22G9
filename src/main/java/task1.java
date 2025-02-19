1bc1745d009fbaa063f1fa439757216bf351543b
samiasam2023 <samiasam2023@gmail.com>
2025-02-18 21:22:00 -0500

    1st and 2nd task done

--- src/main/java/task1.java
+++ src/main/java/task1.java
@@ -5,5 +5,19 @@ public class task1 {
         //Create a program that uses an array to store a list of temperatures for a week, and then uses a loop
         //to find the highest and lowest temperature for the week.
 
+        double [] temperature={97.0, 88.0, 66.0};
+
+        double highTemp=temperature[0];
+        double lowtemp=temperature[0];
+        for (int t=0; t<temperature.length; t++){
+            if(temperature[t]>highTemp){
+                highTemp=temperature[t];
+            }
+            if (temperature[t]<lowtemp){
+                lowtemp=temperature[t];
+            }
+        }
+        System.out.println("Highest temperature : "+highTemp);
+        System.out.println("Lowest temperature : "+lowtemp);
     }
 }
--- src/main/java/task2.java
+++ src/main/java/task2.java
@@ -4,6 +4,11 @@ public class task2 {
 
         //Create an array of integer values. After the array is created, calculate the sum of all stored elements
         //in that array
-
+        int [] values={100, 56, 85, 45, 200};
+        int sum=0;
+        for (int add: values) {
+            sum += add;
+        }
+        System.out.println(sum);
     }
 }
