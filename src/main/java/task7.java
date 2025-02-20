de70603a7566b754bf7f5305f9d7bca81e4d10bb
wasima21 <wasim.aahmed21@gmail.com>
2025-02-20 00:53:00 -0500

    task 7 complete

--- src/main/java/task7.java
+++ src/main/java/task7.java
@@ -4,5 +4,15 @@ public class task7 {
 
         //Write a Java Program to print the first 10 numbers of the Fibonacci series.
 
+        int n = 10, firstNum = 0, secondNum = 1;
+
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(firstNum + " ");
+            int nextNum = firstNum + secondNum;
+            firstNum = secondNum;
+            secondNum = nextNum;
+        }
+
     }
 }
